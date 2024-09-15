323f4493ca52e6b15e18d5d062504e983d99fb53
rakusan <6066112+rakusan@users.noreply.github.com>
2024-09-15 12:17:01 +0900

    remove commnet

--- SpaceSharingExampleVisionOS/SpaceSharingExampleVisionOS/ImmersiveView.swift
+++ SpaceSharingExampleVisionOS/SpaceSharingExampleVisionOS/ImmersiveView.swift
@@ -15,12 +15,8 @@ struct ImmersiveView: View {
 
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
             if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
                 content.add(immersiveContentEntity)
-
-                // Put skybox here.  See example in World project available at
-                // https://developer.apple.com/
             }
         }
         .gesture(
